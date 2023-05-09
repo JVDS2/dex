ae3ba3bd790e7a1e381bcd89cf4a5a1fc5694a7a
JVDS <71608479+JVDS2@users.noreply.github.com>
2023-05-09 18:50:21 -0300

    sfx

--- js/pokeapi.js
+++ js/pokeapi.js
@@ -1,95 +1,161 @@
 function capeOpen(){
-    let cape = document.getElementById("cape");
-    let extra = document.getElementById("extra-info");
-    cape.classList.toggle("opened");
-    extra.classList.toggle("opened");
+  let cape = document.getElementById("cape");
+  let extra = document.getElementById("extra-info");
+  if(cape.classList.contains("opened")){
+    playSound("closeAudio")
+  }
+  else{
+    playSound("openAudio");
+  }
+  cape.classList.toggle("opened");
+  extra.classList.toggle("opened");
+}
+
+function playSound(audioID){
+  var sound = document.getElementById(audioID);
+  sound.play();
 }
 
 function getTypeColor(pokeType){
   const pokemonColors = {
-    "normal": "#A8A77A",
-    "fire": "#EE8130",
-    "water": "#6390F0",
-    "electric": "#F7D02C",
-    "grass": "#7AC74C",
-    "ice": "#96D9D6",
-    "fighting": "#C22E28",
-    "poison": "#A33EA1",
-    "ground": "#E2BF65",
-    "flying": "#A98FF3",
-    "psychic": "#F95587",
-    "bug": "#A6B91A",
-    "rock": "#B6A136",
-    "ghost": "#735797",
-    "dragon": "#6F35FC",
-    "dark": "#705746",
-    "steel": "#B7B7CE",
-    "fairy": "#D685AD"
+    "normal": {
+      primary: "#A8A77A",
+      highlight: "#f7f7e9"
+    },
+    "fire": {
+      primary: "#EE8130",
+      highlight: "#5e2d08"
+    },
+    "water": {
+      primary: "#6390F0",
+      highlight: "#ecf1fe"
+    },
+    "electric": {
+      primary: "#F7D02C",
+      highlight: "#5c5300"
+    },
+    "grass": {
+      primary: "#7AC74C",
+      highlight: "#edf6e4"
+    },
+    "ice": {
+      primary: "#96D9D6",
+      highlight: "#29706d"
+    },
+    "fighting": {
+      primary: "#C22E28",
+      highlight: "#ff9999"
+    },
+    "poison": {
+      primary: "#A33EA1",
+      highlight: "#e3c4e3"
+    },
+    "ground": {
+      primary: "#E2BF65",
+      highlight: "#705015"
+    },
+    "flying": {
+      primary: "#A98FF3",
+      highlight: "#433852"
+    },
+    "psychic": {
+      primary: "#F95587",
+      highlight: "#ffbdce"
+    },
+    "bug": {
+      primary: "#A6B91A",
+      highlight: "#3b3d0f"
+    },
+    "rock": {
+      primary: "#B6A136",
+      highlight: "#494213"
+    },
+    "ghost": {
+      primary: "#735797",
+      highlight: "#d4c9d4"
+    },
+    "dragon": {
+      primary: "#6F35FC",
+      highlight: "#c6b1fb"
+    },
+    "dark": {
+      primary: "#705746",
+      highlight: "#382b24"
+    },
+    "steel": {
+      primary: "#B7B7CE",
+      highlight: "#515161"
+    },
+    "fairy": {
+      primary: "#D685AD",
+      highlight: "#ff1a66"
+    }
   };
+  
   if(pokeType in pokemonColors){
-    const pokeColor = pokemonColors[pokeType];
-    return pokeColor;
+    const primaryColor = pokemonColors[pokeType].primary;
+    const highlightColor = pokemonColors[pokeType].highlight;
+    return [primaryColor, highlightColor];
   }
 }
 
 
 function search(){
-    const input = document.getElementById("input").value;
-    let url = `https://pokeapi.co/api/v2/pokemon/${input.toLowerCase()}`
+  const input = document.getElementById("input").value;
+  let url = `https://pokeapi.co/api/v2/pokemon/${input.toLowerCase()}`
+  
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+          return response.json();
+      }
+      throw new Error('Erro ao fazer requisição');
+    })
+    .then(data => {
+      let pokemonName = data.name;
+      pokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1).toLowerCase();
+  
+      document.getElementById("name").innerHTML = pokemonName;
+      let firstTypeID = document.getElementById("first-type");
+      let secondTypeID = document.getElementById("second-type");
+      let typeLen = data["types"].length;
+
+      let firstType = data["types"]["0"]["type"]["name"];
+      
+      firstTypeID.innerHTML = firstType;
+      firstTypeID.style.background=getTypeColor(firstType)[0];
+      firstTypeID.style.color=getTypeColor(firstType)[1];
     
-    fetch(url)
-      .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-        throw new Error('Erro ao fazer requisição');
-      })
-      .then(data => {
-        let pokemonName = data.name;
-        pokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1).toLowerCase();
-    
-        document.getElementById("name").innerHTML = pokemonName;
-        
-        let typeLen = data["types"].length;
-        let firstType = data["types"]["0"]["type"]["name"];
-        document.getElementById("first-type").innerHTML = firstType;
-        document.getElementById("first-type").style.background=getTypeColor(firstType);
 
-        if(typeLen >= 2){
-            let secondType = data["types"]["1"]["type"]["name"];
-            document.getElementById("second-type").style.display="block";
-            document.getElementById("second-type").innerHTML = secondType;
-            document.getElementById("second-type").style.background = getTypeColor(secondType)
-        }
-        else{
-            document.getElementById("second-type").style.display="none";
-        }
+      if(typeLen >= 2){
+          let secondType = data["types"]["1"]["type"]["name"];
+          secondTypeID.style.display="block";
+          secondTypeID.innerHTML = secondType;
+          secondTypeID.style.background = getTypeColor(secondType)[0];
+          secondTypeID.style.color = getTypeColor(secondType)[1];
+      }
+      else{
+          document.getElementById("second-type").style.display="none";
+      }
 
-        let image = data["sprites"]["other"]["home"]["front_default"];
-        // document.getElementById("image").src = image;
-        let bgImage = `url(${image})`;
-        document.getElementById("image").style.backgroundImage=bgImage;
+      let image = data["sprites"]["other"]["home"]["front_default"];
+      // document.getElementById("image").src = image;
+      let bgImage = `url(${image})`;
+      document.getElementById("image").style.backgroundImage=bgImage;
 
-        let hp = data["stats"]["0"]["base_stat"];
-        let atk = data["stats"]["1"]["base_stat"];
-        let def = data["stats"]["2"]["base_stat"];
-        let spatk = data["stats"]["3"]["base_stat"];
-        let spdef = data["stats"]["4"]["base_stat"];
-        let speed = data["stats"]["5"]["base_stat"];
+      document.getElementById("hp").innerHTML=data["stats"]["0"]["base_stat"];
+      document.getElementById("atk").innerHTML=data["stats"]["1"]["base_stat"];
+      document.getElementById("defense").innerHTML=data["stats"]["2"]["base_stat"];
+      document.getElementById("spatk").innerHTML=data["stats"]["3"]["base_stat"];
+      document.getElementById("spdef").innerHTML=data["stats"]["4"]["base_stat"];
+      document.getElementById("speed").innerHTML=data["stats"]["5"]["base_stat"];
 
-        document.getElementById("hp").innerHTML=hp
-        document.getElementById("atk").innerHTML=atk
-        document.getElementById("defense").innerHTML=def
-        document.getElementById("spatk").innerHTML=spatk
-        document.getElementById("spdef").innerHTML=spdef
-        document.getElementById("speed").innerHTML=speed
-
-
-
-      })
-      .catch(error => {
-        document.getElementById("name").innerHTML="ERRO";
-      });
-    document.getElementById("input").value="";
-        
+    })
+    .catch(error => {
+      console.error(error);
+      document.getElementById("name").innerHTML="ERRO";
+    });
+  document.getElementById("input").value="";
+  playSound("foundedAudio");
+      
 }
